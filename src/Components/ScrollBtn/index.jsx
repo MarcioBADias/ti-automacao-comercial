8c33ff5493599a01f9f943435987e38c151ad30f
unknown <marcio.b.a.dias@gmail.com>
2025-03-07 16:52:51 -0300

    💹 Configurando o Scroll

--- src/Components/ScrollBtn/index.jsx
+++ src/Components/ScrollBtn/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Button } from './styles'
+import { Link } from 'react-scroll'
 
 const ScrollBtn = ({ text }) => (
+    <Link
+    to="contacts"
+          spy={true}
+          smooth={true}
+          offset={-80}
+          duration={800}
+    >
     <Button type="submit">{text}</Button>
+    </Link>
 )
 
 export { ScrollBtn }
